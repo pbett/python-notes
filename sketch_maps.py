24f306a6ce8ff83b5c46ccccb3efe0c1939aa955
Philip Bett <dph3peb1@gmail.com>
2018-05-19 11:17:53 +0100

    Create sketch_maps.py
    Adding code snippets giving examples for simple sketch maps with cartopy.

---
+++ sketch_maps.py
@@ -0,0 +1,55 @@
+'''
+Illustrating basic "sketch-maps", without gridded data.
+e.g. http://scitools.org.uk/cartopy/docs/v0.13/matplotlib/intro.html
+'''
+import cartopy
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+#----------------------------------------------------
+
+# Simplest:
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()  ;  plt.show()  ;  plt.close()
+
+
+# A bit nicer:
+ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=10))
+ax.coastlines()
+plt.tight_layout()
+plt.show()  ;  plt.close()
+
+
+# With a stock image:
+ax = plt.axes(projection=ccrs.Mollweide(central_longitude=10))
+ax.stock_img()
+plt.tight_layout()
+plt.show()  ;  plt.close()
+
+
+# Just simple colouring of land and sea:
+ax = plt.axes(projection=ccrs.Robinson(central_longitude=10))
+ax.add_feature(cartopy.feature.LAND)
+ax.add_feature(cartopy.feature.OCEAN)
+ax.coastlines(color="grey")  # Or you could do:
+#ax.add_feature(cartopy.feature.COASTLINE)
+plt.tight_layout()
+plt.show()  ;  plt.close()
+
+
+
+
+
+proj = ccrs.Orthographic(central_longitude=0.0, central_latitude=50.0)
+ax = plt.axes(projection=proj)
+ax.add_feature(cartopy.feature.LAND)
+ax.add_feature(cartopy.feature.OCEAN)
+ax.add_feature(cartopy.feature.COASTLINE) #, edgecolor="brown")
+ax.gridlines()
+plt.tight_layout()
+plt.show()  ;  plt.close()
+# Other features are BORDERS, LAKES, RIVERS.
+# http://scitools.org.uk/cartopy/docs/v0.13/matplotlib/feature_interface.html
+
+
+
+
